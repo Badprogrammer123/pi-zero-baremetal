8f459fcf864c61910e5ea2f267024941550df8c9
Badprogrammer123 <76971278+Badprogrammer123@users.noreply.github.com>
2026-06-27 20:20:26 -0400

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,66 +1,25 @@
-#include <stdint.h>
+#include "mini_uart.h"
+#include <string.h>
 
-#define PERIPHERAL_BASE 0x3F000000 
-#define GPIO_BASE 0x3F200000 
 
-#define GPFSEL2 (GPIO_BASE + 0x08)
-#define GPCLR0 (GPIO_BASE + 0x28)
-#define GPSET0 (GPIO_BASE + 0x1c)
 
-#define UART1_BASE 0x3F215000
-#define GPFSEL1 0x3F200004
+
+
+set_mini_uart test = {
+	.baud_rate = 115200,
+	.tx_enable = AUX_MU_CNTL_TX_ENABLE,
+	.rx_enable = AUX_MU_CNTL_RX_ENABLE ,	
+};
+
+
 
 void kernel_main(){
-
-		//GPIO pins set to ALT5
-        volatile unsigned int *uart_regs = (volatile unsigned int *) UART1_BASE;
-    	volatile unsigned int *set_pin_mode = (volatile unsigned int *) GPFSEL1;
-    	volatile unsigned int *set_test =  (volatile unsigned int *) GPSET0;
-		*set_pin_mode &= ~(63 << 12);
-        *set_pin_mode |= (1 << 13);
-		*set_pin_mode |= (1 << 16);
+	mini_data_set data;
+	test.data_settings = &data;
+       	test.data_settings->size = 1; 
+	volatile unsigned int *regs = set_uart(&test); 
+	transmit_send(regs, strlen("hello my name is florant"), "hello my name is florant"); 
 
 
-        volatile uint32_t baudrate = 250000000 / (8 * (115200 + 1));
 
-
-        //mini uart needs to be set to 1 to be able to write to its registers 
-        *(uart_regs + (0x4/4)) |= 1;
-
-        //disable transmit and recv
-        *(uart_regs + (0x60/4)) &= ~3;
-
-
-        //clear DLAB access 
-        *(uart_regs + (0x4C/4)) &= ~(1 << 7);
-
-        //enable FIFO interrupt register
-        *(uart_regs +  (0x44/4)) |= ~3;
-
-        //uart baud rate
-        *(uart_regs) |= baudrate;
-
-        //set data register size to 8 bits
-        *(uart_regs + (0x4C/4)) |= 1;
-
-
-		//clear uart receive
-		*(uart_regs + (0x48/4)) |= 2;
-
-    	//transmitter and Receiver set
-    	*(uart_regs + (0x60/4)) |= 3;
-	
-		//clearing the receiver fifo
-		*(uart_regs + (0x40/4)) &= 0; 
-		while(1){
-			//check if receive fifo holds 8 symbols if so pop each one form the fifo and write it into the data register to be transmitted. 
-			if (((((*(uart_regs + (0x64/4))) >> 16) & 0x0F) + '0') == '8'){
-				for(int i = 0; i < 8; i ++){
-					char buffer = (char)(*(uart_regs + (0x40/4)));
-					*(uart_regs + (0x40/4)) = buffer;
-				}
-			}	
-	
-		}
-	
 }
---
+++ mini_uart.h
@@ -0,0 +1,121 @@
+#ifndef MINI_UART_H
+#define MINI_UART_H
+
+
+
+/* Base addresses */
+
+#define AUX_BASE        0x3F215000
+#define GPIO_BASE       0x3F200000
+#define GPFSEL1 	0x3F200004
+
+/* Mini UART register offsets */
+
+#define AUX_ENABLE_REG  (0x04/4)
+#define AUX_MU_IO_REG   (0x40/4)
+#define AUX_MU_IER_REG  (0x44/4)
+#define AUX_MU_IIR_REG  (0x48/4)
+#define AUX_MU_LCR_REG  (0x4C/4)
+#define AUX_MU_MCR_REG  (0x50/4)
+#define AUX_MU_LSR_REG  (0x54/4)
+#define AUX_MU_MSR_REG  (0x58/4)
+#define AUX_MU_SCRATCH  (0x5C/4)
+#define AUX_MU_CNTL_REG (0x60/4)
+#define AUX_MU_STAT_REG (0x64/4)
+#define AUX_MU_BAUD_REG (0x68/4)
+
+/* GPIO PINS  */
+#define GPIO14_TX_MODE		 (1 << 13) 
+#define GPIO15_RX_MODE		 (1 << 16)
+#define GPIO14_CLEAR		 (7 << 12) 
+#define GPIO15_CLEAR 		 (7 << 15)
+
+/* AUX UART1 ENABLE */
+#define AUX_ENABLE_UART1	 (1 << 0)
+
+/* TX and RX ENABLE */ 
+#define AUX_MU_CNTL_RX_ENABLE	 (1 << 0)
+#define AUX_MU_CNTL_TX_ENABLE 	 (1 << 1)
+
+/* Mini UART IIR bits */
+#define AUX_MU_IIR_CLEAR_RX_FIFO (1 << 1)
+#define AUX_MU_IIR_CLEAR_TX_FIFO (1 << 2)
+#define AUX_MU_IIR_FIFO_ENABLE   (1 << 0)
+#define AUX_MU_IIR_FIFO_DISABLE  (3 << 6)
+
+#endif
+
+typedef struct mini_uart_data_settings {
+	volatile unsigned int size; 
+
+}mini_data_set;
+
+
+typedef struct mini_uart_settings{
+	unsigned int baud_rate;
+	unsigned int tx_enable;
+       	unsigned int rx_enable;	
+	unsigned fifo_disable; 
+	struct mini_uart_data_settings *data_settings; 
+}set_mini_uart;
+
+
+void transmit_send(volatile unsigned int * uart_regs, int char_count, char *str ){
+	int i = 0;
+	while(char_count != 0){
+		if( ((*(uart_regs + (AUX_MU_LSR_REG /4))) & (1 << 5)) != 0 ){
+			*(uart_regs + (AUX_MU_IO_REG/4)) = str[i];
+			i ++;
+			char_count --;
+		}
+	}	
+
+
+
+}
+
+void receive(volatile unsigned int * uart_regs){
+//have to finish should I store stuff in ram what do I do other than clear as it gets full	
+}
+
+
+
+volatile unsigned int *set_uart(set_mini_uart * mini_set){
+        //GPIO pins set to ALT5
+
+        volatile unsigned int *uart_regs = (volatile unsigned int *) AUX_BASE;
+        volatile unsigned int *set_pin_mode = (volatile unsigned int *) GPFSEL1;
+        volatile unsigned int  baudrate = 250000000 / (8 * (mini_set->baud_rate + 1));
+	
+	//Clear GPIO 14 and 15
+        *set_pin_mode &= ~(GPIO14_CLEAR | GPIO15_CLEAR);
+	
+	//ALT5 GPIO 14 and 15 (RX_UART1 and TX_UART1) 
+        *set_pin_mode |= GPIO14_TX_MODE ;
+        *set_pin_mode |= GPIO15_RX_MODE ;
+        
+
+	//Enable UART1 required to access and configure registers
+	*(uart_regs + AUX_ENABLE_REG) |= AUX_ENABLE_UART1;
+        
+	//Disable tx and rx to config
+        *(uart_regs + AUX_MU_STAT_REG) &= ~(AUX_MU_CNTL_RX_ENABLE  | AUX_MU_CNTL_RX_ENABLE );
+	
+        *(uart_regs) |= baudrate;
+
+
+        //set data register size to 8 bits
+        *(uart_regs + AUX_MU_LCR_REG) |= mini_set->data_settings->size;
+	
+
+        *(uart_regs + AUX_MU_IIR_REG) &= mini_set->fifo_disable == 0 ? AUX_MU_IIR_FIFO_ENABLE : ~(AUX_MU_IIR_FIFO_DISABLE);
+	
+	*(uart_regs + AUX_MU_IIR_REG) |= mini_set->fifo_disable == 0 ? AUX_MU_IIR_CLEAR_TX_FIFO | AUX_MU_IIR_CLEAR_RX_FIFO : 0; 
+
+        *(uart_regs + AUX_MU_CNTL_REG) |= mini_set->tx_enable | mini_set->rx_enable;
+
+	return uart_regs; 
+	
+}
+
+
